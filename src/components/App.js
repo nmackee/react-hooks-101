0e1cf0a2f2938334f3acd7b44c5825e0f8d618ab
nmackee <mackee.base@gmail.com>
2022-02-14 22:54:18 +0900

    refactor components

--- src/components/App.js
+++ src/components/App.js
@@ -1,66 +1,25 @@
-import React, { useReducer, useState } from 'react';
+import React, { useReducer } from 'react';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import Event from './Event'
-import reducer from '../reducers'
+import EventForm from './EventForm'
 
+
+import Events from './Events'
+import reducer from '../reducers'
 
 const App = () =>  {
   const [state, dispatch] = useReducer(reducer, [])
-
-  const [title, setTitle] = useState('')
-  const [body, setBody ] = useState('')
-
-  const addEvent = e => {
-    e.preventDefault()
-    // console.log({title, body})
-    dispatch({
-      type: 'CREATE_EVENT',
-      title,
-      body
-    })
-
-    setTitle('')
-    setBody('')
-  }
-  console.log({state})
+  // console.log(state, ' in App.js')
   
   return (
+    <>
     <div className="container-fluid">
-      <h4>イベント作成フォーム</h4>
-      <form>
-        <div className='form-group'>
-          <label htmlFor='formEventTitle'>タイトル</label>
-          <input className='form-control' id='formEventTitle' value={title} onChange={e => setTitle(e.target.value)} />
-        </div>
-        
-        <div className='form-group'>
-          <label htmlFor='formEventBody'>ボディー</label>
-          <input className='form-control' id='formEventBody' value={body} onChange={e => setBody(e.target.value)} />
-        </div>
-
-        <button className='btn btn-primary' onClick={addEvent}>イベントを作成する</button>
-        <button className='btn btn-danger'>すべてのイベントを削除する</button>
-      </form>
-
-      <h4>イベント一覧</h4>
-      <table className='table table-hover'>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>タイトル</th>
-            <th>ボディー</th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          { state.map(event => (<Event key={event.id} event={event} dispatch={dispatch}/>))}
-          
-        </tbody>
-      </table>
+      <EventForm state={state} dispatch={dispatch}/>
+      <Events state={state} dispatch={dispatch}/>
     </div>
-  );
+    </>
+  )
 }
 
 export default App;
